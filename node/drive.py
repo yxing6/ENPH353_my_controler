a0099f8e5b12b42831221d961529f448f7bebf55
Conner <connerjf03@gmail.com>
2024-03-28 09:55:30 -0700

    Line following changes

--- node/drive.py
+++ node/drive.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 import rospy
-import cv2 as cv
+import cv2
 import numpy as np
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
@@ -24,13 +24,6 @@ class Drive:
         # Create a bridge between ROS and OpenCV
         self.bridge = CvBridge()
 
-        # control parameters
-        self.Kp = 0.02  # Proportional gain
-        self.error_threshold = 20  # drive with different linear speed wrt this error_theshold
-        self.linear_val_max = 0.7  # drive fast when error is small
-        self.linear_val_min = 0.5  # drive slow when error is small
-        self.mid_x = 0.0  # center of the frame initialized to be 0, updated at each find_middle function call
-
     def image_callback(self, data):
 
         # process the scribed image from camera in openCV 
@@ -42,60 +35,87 @@ class Drive:
             return
 
         # Create Twist message and publish to cmd_vel
-        twist_msg = Twist()
-        speed = self.calculate_speed(cv_image)
-        print("speed: ", speed)
-        twist_msg.linear.x = speed[0]
-        twist_msg.angular.z = speed[1]
+        twist_msg = self.tape_follow(cv_image)
+        print("speed: ", twist_msg.linear.x)
+        print("angular: ", twist_msg.angular.z)
         self.cmd_vel_pub.publish(twist_msg)
 
-    def calculate_speed(self, img):
+    def tape_follow(self, img):
 
-        dim_x = img.shape[1]
+        cropped_img = img[int(img.shape[0]/2):int(img.shape[0])]
+        # Convert the frame to grayscale
+        gray_frame = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
+        x_len = len(cropped_img[0])
+        y_len = len(cropped_img)
 
-        # detect_line calculates and modifies the target center
-        self.find_middle(img)
-        # angular error is the different between the center of the frame and targer center
-        angular_error = dim_x / 2 - self.mid_x
-        angular_vel = self.Kp * angular_error
+        # Define the kernel size for erosion and dilation
+        kernel_size = 5
+        kernel = np.ones((kernel_size, kernel_size), np.uint8)
 
-        if abs(angular_error) <= self.error_threshold:
-            linear_vel = self.linear_val_max
+        # Apply a black and white threshold
+        (thresh, black_white_frame) = cv2.threshold(gray_frame, 83, 255, cv2.THRESH_BINARY)
+
+        # Apply dilation
+        dilation = cv2.dilate(black_white_frame, kernel, iterations=1)
+
+        # Apply erosion
+        erosion = cv2.erode(dilation, kernel, iterations=3)
+
+        # Invert colours to correct findContours
+        inverted_erosion = cv2.bitwise_not(erosion)
+
+        left_boundary = int(x_len/2)
+        right_boundary = int(x_len/2)
+
+        for pixel_index in range(x_len):
+            if inverted_erosion[y_len-1, pixel_index] > 0:
+                left_boundary = pixel_index
+                break
+        
+        for pixel_index in reversed(range(x_len)):
+            if inverted_erosion[y_len-1, pixel_index] > 0:
+                right_boundary = pixel_index
+                break
+        
+        mid_of_boundaries = int((left_boundary+right_boundary)/2)
+
+        contours, hierarchy = cv2.findContours(inverted_erosion, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(contours) > 0:
+            x_midpoint = int(len(inverted_erosion[0])/2)
+            y_midpoint = int(len(inverted_erosion)/2)
+            index = 0
+
+            # If there is more than 1 contour, take the lowest one
+            if len(contours) > 1:
+                lowest_box = 0
+                index = 0
+                for i in range(len(contours)):
+                    rect_x, rect_y, rect_w, rect_h = cv2.boundingRect(contours[i])
+                    box_y = rect_y + rect_h
+                    if box_y > lowest_box:
+                        lowest_box = box_y
+                        index = i
+
+            # Create a minimum area rectangle around the contour
+            (rect_x, rect_y), (rect_w, rect_h), angle = cv2.minAreaRect(contours[index])
+
+            # Calculate the horizontal distance from the center of the box to the center of the frame
+            #averaged_x_midpoint = (mid_of_boundaries + rect_x) / 2
+            averaged_x_midpoint = mid_of_boundaries
+            error = averaged_x_midpoint - x_midpoint
+            move = Twist()
+            move.linear.x = 1 - (rect_y / (y_midpoint * 2))
+            move.angular.z = -1 * error / x_midpoint
+
+            gray_frame = cv2.circle(gray_frame, (int(rect_x), int(rect_y)), 16, 60, -1)
+            gray_frame = cv2.circle(gray_frame, (int(mid_of_boundaries), int(y_midpoint)), 16, 20, -1)
+            cv2.imshow("image", gray_frame)
+            cv2.waitKey(3)
+
+            return move
         else:
-            linear_vel = self.linear_val_min
-
-        return linear_vel, angular_vel
-
-    def find_middle(self, img):
-
-        # image processing: 
-        # change the frame to grey scale
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-
-        # blur it so the "road" far away become less easy to detect
-        # kernel_size: Gaussian kernel size
-        # sigma_x: Gaussian kernel standard deviation in X direction
-        # sigma_y: Gaussian kernel standard deviation in Y direction
-        kernel_size = 13
-        sigma_x = 5
-        sigma_y = 5
-        blur_gray = cv.GaussianBlur(gray, (kernel_size, kernel_size), sigma_x, sigma_y)  # gray scale the image
-
-        # binary it
-        ret, binary = cv.threshold(blur_gray, 70, 255, cv.THRESH_BINARY)
-
-        cv.imshow("name", binary)
-        cv.waitKey(3)
-
-        last_row = binary[-1, :]
-        print(last_row)
-
-        if np.any(last_row == 0):
-            last_list = last_row.tolist()
-            first_index = last_list.index(0)
-            last_index = len(last_list) - 1 - last_list[::-1].index(0)
-            new_mid = (first_index + last_index) / 2
-            self.mid_x = new_mid
+            return Twist()
 
 
 def main():
@@ -103,7 +123,7 @@ def main():
         image_subscriber = Drive()
         rospy.spin()
     except rospy.ROSInterruptException:
-        cv.destroyAllWindows()
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
